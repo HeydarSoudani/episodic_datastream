36fc2b0f1671604581024faabff0879f33f10205
Heydar Soudani <heydar.soudani@gmail.com>
2021-11-14 10:51:03 +0330

    improve output file

--- stream_learn.py
+++ stream_learn.py
@@ -30,87 +30,92 @@ def stream_learn(model,
   detection_results = []
   total_results = []
   last_idx = 0
-  for i, data in enumerate(dataloader):
-    
-    model.eval()
-    with torch.no_grad():
-      sample, label = data
-      sample, label = sample.to(device), label.to(device)
-      _, feature = model.forward(sample)
-      real_novelty = label.item() not in detector._known_labels
-      detected_novelty, predicted_label, prob = detector(feature)      
-      detection_results.append((label.item(), predicted_label, real_novelty, detected_novelty))
 
-      sample = torch.squeeze(sample, 0) #[1, 28, 28]
-      if detected_novelty:
-        unknown_buffer.append((sample, label))
-      else:
-        known_buffer[predicted_label].append((sample, label))
+  with open('output.txt', 'w') as f:
+    for i, data in enumerate(dataloader):
+      
+      model.eval()
+      with torch.no_grad():
+        sample, label = data
+        sample, label = sample.to(device), label.to(device)
+        _, feature = model.forward(sample)
+        real_novelty = label.item() not in detector._known_labels
+        detected_novelty, predicted_label, prob = detector(feature)      
+        detection_results.append((label.item(), predicted_label, real_novelty, detected_novelty))
 
-      if (i+1) % 100 == 0:
-        print("[stream %5d]: %d, %2d, %7.4f, %5s, %5s, %d"%
-          (i+1, label, predicted_label, prob, real_novelty, detected_novelty, len(unknown_buffer)))
-    
-    if (i+1) % args.known_retrain_interval == 0 \
-       or len(unknown_buffer) == args.buffer_size:
-      print('=== Retraining... ===')
-      ### == Preparing buffer ==================
-      if (i+1) % args.known_retrain_interval == 0:
-        buffer = []
-        for label , data in known_buffer.items():
-          n = len(data)
-          idxs = np.random.choice(range(n), size=args.known_per_class, replace=False)
-          buffer.extend([data[i] for i in idxs])
-      
-      elif len(unknown_buffer) == args.buffer_size:
-        buffer = unknown_buffer
+        sample = torch.squeeze(sample, 0) #[1, 28, 28]
+        if detected_novelty:
+          unknown_buffer.append((sample, label))
+        else:
+          known_buffer[predicted_label].append((sample, label))
 
-      ### == 1) evaluation ======================
-      sample_num = i-last_idx
+        if (i+1) % 100 == 0:
+          print("[stream %5d]: %d, %2d, %7.4f, %5s, %5s, %d"%
+            (i+1, label, predicted_label, prob, real_novelty, detected_novelty, len(unknown_buffer)))
       
-      M_new, F_new, CwCA, OwCA, cm = evaluate(detection_results, detector._known_labels)
-      print("[On %5d samples]: %7.4f, %7.4f, %7.4f, %7.4f"%
-        (sample_num, CwCA, OwCA, M_new, F_new))
-      print("confusion matrix: \n%s"% cm)
-      
-      ### == 2) Preparing retrain data ==========
-      new_train_data = memory.select(buffer, return_data=True)
-      
-      ### == 3) Retraining Model ================
-      train(model, new_train_data, args, device)
-      
-      ### == 4) Recalculating Detector ==========
-      print("Calculating detector ...")
-      samples, prototypes, intra_distances = detector_preparation(model, new_train_data, args, device)
-      new_labels = list(prototypes.keys())
-
-      detector.threshold_calculation(intra_distances,
-                                     prototypes,
-                                     new_labels,
-                                     args.std_coefficient)
-      print("Detector Threshold: {}".format(detector.thresholds))  
-      detector.save(args.detector_path)
-      print("Detector has been saved in {}.".format(args.detector_path))
-
-      ### == 5) Update parameters ===============
-      if len(unknown_buffer) == args.buffer_size:
-        known_labels = list(known_buffer.keys())
-        labels_diff = list(set(new_labels)-set(known_labels))
-        if len(labels_diff) != 0:
-          for label in labels_diff:
-            known_buffer[label] = []
+      if (i+1) % args.known_retrain_interval == 0 \
+        or len(unknown_buffer) == args.buffer_size:
+        print('=== Retraining... =================')
+        ### == Preparing buffer ==================
+        if (i+1) % args.known_retrain_interval == 0:
+          buffer = []
+          for label , data in known_buffer.items():
+            n = len(data)
+            idxs = np.random.choice(range(n), size=args.known_per_class, replace=False)
+            buffer.extend([data[i] for i in idxs])
         
-        unknown_buffer.clear()
+        elif len(unknown_buffer) == args.buffer_size:
+          buffer = unknown_buffer
 
-      if (i+1) % args.known_retrain_interval == 0:
-        known_buffer = {i:[] for i in detector._known_labels}
+        ### == 1) evaluation ======================
+        sample_num = i-last_idx
+        
+        M_new, F_new, CwCA, OwCA, cm = evaluate(detection_results, detector._known_labels)
+        print("[On %5d samples]: %7.4f, %7.4f, %7.4f, %7.4f"%
+          (sample_num, CwCA, OwCA, M_new, F_new))
+        print("confusion matrix: \n%s"% cm)
+        
+        ### == 2) Preparing retrain data ==========
+        new_train_data = memory.select(buffer, return_data=True)
+        
+        ### == 3) Retraining Model ================
+        train(model, new_train_data, args, device)
+        
+        ### == 4) Recalculating Detector ==========
+        print("Calculating detector ...")
+        samples, prototypes, intra_distances = detector_preparation(model, new_train_data, args, device)
+        new_labels = list(prototypes.keys())
+
+        detector.threshold_calculation(intra_distances,
+                                      prototypes,
+                                      new_labels,
+                                      args.std_coefficient)
+        print("Detector Threshold: {}".format(detector.thresholds))  
+        detector.save(args.detector_path)
+        print("Detector has been saved in {}.".format(args.detector_path))
+
+        ### == 5) Update parameters ===============
+        if len(unknown_buffer) == args.buffer_size:
+          known_labels = list(known_buffer.keys())
+          labels_diff = list(set(new_labels)-set(known_labels))
+          if len(labels_diff) != 0:
+            for label in labels_diff:
+              known_buffer[label] = []
+          
+          unknown_buffer.clear()
+
+        if (i+1) % args.known_retrain_interval == 0:
+          known_buffer = {i:[] for i in detector._known_labels}
+        
       
-      with open('output.txt', 'w') as f:
         f.write("[On %5d samples]: %7.4f, %7.4f, %7.4f, %7.4f \n"%
           (sample_num, CwCA, OwCA, M_new, F_new))
-      total_results.append((i-last_idx, M_new, F_new, CwCA, OwCA))
-      detection_results.clear()
-      last_idx = i
-      time.sleep(3)
-  
+        total_results.append((i-last_idx, M_new, F_new, CwCA, OwCA))
+        detection_results.clear()
+        last_idx = i
+        
+        print('=== Streaming... =================')
+        time.sleep(3)
+
+    
   print(total_results)
