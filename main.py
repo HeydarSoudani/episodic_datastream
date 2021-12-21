cb2a8b44534a1d970ddf4eb31e1e1fa9b36905c6
Heydar Soudani <heydar.soudani@gmail.com>
2021-12-21 10:40:07 +0330

    add pmnist

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pandas import read_csv
 
 from datasets.dataset import SimpleDataset
 from models.cnn import Conv_4
-from models.mlp import MLP
+from models.mlp import MLP, Xavier
 
 from utils.memory_selector import OperationalMemory, IncrementalMemory
 from detectors.pt_detector import PtDetector
