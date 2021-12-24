5fc53aed5aaa4ac2e024113d7324a5d61ffcc138
Heydar Soudani <heydar.soudani@gmail.com>
2021-12-25 02:31:05 +0330

    pfmnist added epis

--- incremental_learn.py
+++ incremental_learn.py
@@ -3,8 +3,8 @@ import os
 import numpy as np
 import pandas as pd
 
-# from trainers.episodic_train import train
-from trainers.batch_train import train, test
+from trainers.episodic_train import train
+# from trainers.batch_train import train, test
 from datasets.dataset import SimpleDataset
 from utils.preparation import transforms_preparation
 
@@ -51,15 +51,15 @@ def increm_learn(model,
       # 2) batch training
       #    
     
-    train(model,
-        task_data,
-        args, device)
     # train(model,
-    #     learner,
     #     task_data,
     #     args, device)
+    train(model,
+        learner,
+        task_data,
+        args, device)
 
-    # if task == 0:
+    if task == 0:
       # train(model,
       #   task_data,
       #   args, device)
@@ -69,8 +69,8 @@ def increm_learn(model,
       #       args, device)  
       # = Update memory =====
       # memory.update(task_data)
-      # args.beta_type = 'fixed'
-      # args.beta = 0.999
+      args.beta_type = 'fixed'
+      args.beta = 0.999
     # else:
     #   replay_mem = memory()
     #   train_data = np.concatenate((task_data, replay_mem))
@@ -107,25 +107,25 @@ def increm_learn(model,
                                    shuffle=False)
 
       ### 1) episodic test
-      # known_labels = test_dataset.label_set
-      # # #known_labels = set(range((task+1)*2))
-      # _, acc_dis, acc_cls = learner.evaluate(model,
-      #                                       test_dataloader,
-      #                                       known_labels,
-      #                                       args)
+      known_labels = test_dataset.label_set
+      # #known_labels = set(range((task+1)*2))
+      _, acc_dis, acc_cls = learner.evaluate(model,
+                                            test_dataloader,
+                                            known_labels,
+                                            args)
       ### 2) batch test
-      acc_cls = test(model, test_dataloader, args, device)
+      # acc_cls = test(model, test_dataloader, args, device)
 
-      # tasks_acc_dist[prev_task] = acc_dis
+      tasks_acc_dist[prev_task] = acc_dis
       tasks_acc_cls[prev_task] = acc_cls
     
-    # mean_acc_dist = np.mean(tasks_acc_dist[:task+1])
+    mean_acc_dist = np.mean(tasks_acc_dist[:task+1])
     mean_acc_cls = np.mean(tasks_acc_cls[:task+1])
     
-    # print("Dist acc.: %7.4f, %7.4f, %7.4f, %7.4f, %7.4f \n"% tuple(tasks_acc_dist))
+    print("Dist acc.: %7.4f, %7.4f, %7.4f, %7.4f, %7.4f \n"% tuple(tasks_acc_dist))
     print("Cls  acc.: %7.4f, %7.4f, %7.4f, %7.4f, %7.4f \n"% tuple(tasks_acc_cls))
-    # print('Mean -> Dist: {}, Cls: {}'.format(round(mean_acc_dist, 3), round(mean_acc_cls, 3)))
-    print('Mean -> Cls: {}'.format(round(mean_acc_cls, 3)))
+    print('Mean -> Dist: {}, Cls: {}'.format(round(mean_acc_dist, 3), round(mean_acc_cls, 3)))
+    # print('Mean -> Cls: {}'.format(round(mean_acc_cls, 3)))
     # f.write("%7.4f, %7.4f, %7.4f, %7.4f, %7.4f \n"% tuple(prev_tasks_acc))
     
 
