52ff2465777cdd82df8b2ec7302346e329d0f51b
Heydar Soudani <heydar.soudani@gmail.com>
2021-12-28 11:01:23 +0330

    offline epis

--- init_learn.py
+++ init_learn.py
@@ -13,25 +13,49 @@ def init_learn(model,
   train(model, pt_learner, train_data, args, device)
 
   ## == Save Novel detector ===========
-  print("Calculating detector ...")
-  samples, known_labels, intra_distances\
-    = detector_preparation(model,
-                           pt_learner.prototypes,
-                           train_data,
-                           args, device)
+  # print("Calculating detector ...")
+  # samples, known_labels, intra_distances\
+  #   = detector_preparation(model,
+  #                          pt_learner.prototypes,
+  #                          train_data,
+  #                          args, device)
 
-  detector.threshold_calculation(intra_distances,
-                                 known_labels,
-                                 args.std_coefficient)
-  print("Detector Threshold: {}".format(detector.thresholds))  
-  detector.save(args.detector_path)
-  print("Detector has been saved in {}.".format(args.detector_path))
+  # detector.threshold_calculation(intra_distances,
+  #                                known_labels,
+  #                                args.std_coefficient)
+  # print("Detector Threshold: {}".format(detector.thresholds))  
+  # detector.save(args.detector_path)
+  # print("Detector has been saved in {}.".format(args.detector_path))
   
-  ## == Save Memory selector ==========
-  print("Creating memory ...")
-  memory.select(model, data=samples)
-  memory.save(args.memory_path)
-  print("Memory has been saved in {}.".format(args.memory_path))
+  # ## == Save Memory selector ==========
+  # print("Creating memory ...")
+  # memory.select(model, data=samples)
+  # memory.save(args.memory_path)
+  # print("Memory has been saved in {}.".format(args.memory_path))
+
+
+  ## == Test ==========================
+  test_data = pd.read_csv(
+            os.path.join(args.data_path, args.test_file),
+            sep=',', header=None).values
+
+  if args.use_transform:
+    _, test_transform = transforms_preparation()
+    test_dataset = SimpleDataset(test_data, args, transforms=test_transform)
+  else:
+    test_dataset = SimpleDataset(test_data, args)
+  test_dataloader = DataLoader(dataset=test_dataset,
+                                batch_size=args.batch_size,
+                                shuffle=False)
+
+  known_labels = test_dataset.label_set
+  print('Test on: {}'.format(known_labels))
+  _, acc_dis, acc_cls = pt_learner.evaluate(model,
+                                        test_dataloader,
+                                        known_labels,
+                                        args)
+  print('Dist: {}, Cls: {}'.format(acc_dis, acc_cls))
+
 
 
 if __name__ == '__main__':
