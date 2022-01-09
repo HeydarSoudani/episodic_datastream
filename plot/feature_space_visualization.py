9e095fd68a2634518385ece772d58600a3992ff5
Heydar Soudani <heydar.soudani@gmail.com>
2022-01-10 01:48:45 +0330

    hidden

--- plot/feature_space_visualization.py
+++ plot/feature_space_visualization.py
@@ -78,7 +78,7 @@ def visualization(model, args, device):
   sampler = PtSampler(
     test_dataset,
     n_way=6,
-    n_shot=800,
+    n_shot=1000,
     n_query=0,
     n_tasks=1
   )
@@ -109,29 +109,29 @@ def visualization(model, args, device):
   print(features.shape)
   print(support_labels.shape)
 
-  tsne_plot(features, support_labels, file_name='tsne_init')
+  # tsne_plot(features, support_labels, file_name='tsne_init')
   pca_plot(features, support_labels, file_name='pca_init')
   hausdorff_calculate(features, support_labels)
   
   # == last plot ============================
-  model.load(os.path.join(args.save, 'model_last.pt'))
-  print("Load model from {}".format(os.path.join(args.save, 'model_last.pt')))
+  # model.load(os.path.join(args.save, 'model_last.pt'))
+  # print("Load model from {}".format(os.path.join(args.save, 'model_last.pt')))
 
-  with torch.no_grad():
-    batch = next(iter(test_dataloader))
-    support_images, support_labels, _, _ = batch
-    support_images = support_images.reshape(-1, *support_images.shape[2:])
-    support_labels = support_labels.flatten()
-    support_images = support_images.to(device)
-    support_labels = support_labels.to(device)
+  # with torch.no_grad():
+  #   batch = next(iter(test_dataloader))
+  #   support_images, support_labels, _, _ = batch
+  #   support_images = support_images.reshape(-1, *support_images.shape[2:])
+  #   support_labels = support_labels.flatten()
+  #   support_images = support_images.to(device)
+  #   support_labels = support_labels.to(device)
 
-    outputs, features = model.forward(support_images)
-    features = features.cpu().detach().numpy()
-    support_labels = support_labels.cpu().detach().numpy()
+  #   outputs, features = model.forward(support_images)
+  #   features = features.cpu().detach().numpy()
+  #   support_labels = support_labels.cpu().detach().numpy()
 
-  tsne_plot(features, support_labels, file_name='tsne_last')
-  pca_plot(features, support_labels, file_name='pca_last')
-  hausdorff_calculate(features, support_labels)
+  # # tsne_plot(features, support_labels, file_name='tsne_last')
+  # pca_plot(features, support_labels, file_name='pca_last')
+  # hausdorff_calculate(features, support_labels)
 
 
 
