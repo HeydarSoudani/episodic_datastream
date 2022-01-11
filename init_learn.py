775b7a079dc2f2a7a73355d66337752b5d5fdaf6
Heydar Soudani <heydar.soudani@gmail.com>
2022-01-11 15:12:24 +0330

    adam

--- init_learn.py
+++ init_learn.py
@@ -19,65 +19,65 @@ def init_learn(model,
   ### == Train Model =================
   episodic_train(model, pt_learner, train_data, args, device)
 
-  # == save model for plot ===========
-  model.save(os.path.join(args.save, "model_after_init.pt"))
-  print("= ...model after init saved")
+  # # == save model for plot ===========
+  # model.save(os.path.join(args.save, "model_after_init.pt"))
+  # print("= ...model after init saved")
 
-  # == Save Novel detector ===========
-  print("Calculating detector ...")
-  samples, known_labels, intra_distances\
-    = detector_preparation(model,
-                           pt_learner.prototypes,
-                           train_data,
-                           args, device)
+  # # == Save Novel detector ===========
+  # print("Calculating detector ...")
+  # samples, known_labels, intra_distances\
+  #   = detector_preparation(model,
+  #                          pt_learner.prototypes,
+  #                          train_data,
+  #                          args, device)
 
-  detector.threshold_calculation(intra_distances,
-                                 known_labels,
-                                 args.std_coefficient)
-  print("Detector Threshold: {}".format(detector.thresholds))  
-  detector.save(args.detector_path)
-  print("Detector has been saved in {}.".format(args.detector_path))
+  # detector.threshold_calculation(intra_distances,
+  #                                known_labels,
+  #                                args.std_coefficient)
+  # print("Detector Threshold: {}".format(detector.thresholds))  
+  # detector.save(args.detector_path)
+  # print("Detector has been saved in {}.".format(args.detector_path))
   
-  ## == Save Memory selector ==========
-  print("Creating memory ...")
-  memory.select(data=samples)
-  memory.save(args.memory_path)
-  print("Memory has been saved in {}.".format(args.memory_path))
+  # ## == Save Memory selector ==========
+  # print("Creating memory ...")
+  # memory.select(data=samples)
+  # memory.save(args.memory_path)
+  # print("Memory has been saved in {}.".format(args.memory_path))
 
 
   ## == Test ==========================
-  # test_data = read_csv(
-  #           os.path.join(args.data_path, args.test_file),
-  #           sep=',', header=None).values
-  # print('test_data: {}'.format(test_data.shape))
-  # if args.use_transform:
-  #   _, test_transform = transforms_preparation()
-  #   test_dataset = SimpleDataset(test_data, args, transforms=test_transform)
-  # else:
-  #   test_dataset = SimpleDataset(test_data, args)
-  # test_dataloader = DataLoader(dataset=test_dataset,
-  #                               batch_size=args.batch_size,
-  #                               shuffle=False)
+  test_data = read_csv(
+            os.path.join(args.data_path, args.test_file),
+            sep=',', header=None).values
+  print('test_data: {}'.format(test_data.shape))
+  if args.use_transform:
+    _, test_transform = transforms_preparation()
+    test_dataset = SimpleDataset(test_data, args, transforms=test_transform)
+  else:
+    test_dataset = SimpleDataset(test_data, args)
+  test_dataloader = DataLoader(dataset=test_dataset,
+                                batch_size=args.batch_size,
+                                shuffle=False)
 
-  # known_labels = test_dataset.label_set
+  known_labels = test_dataset.label_set
   
-  # print('Test with last model')
-  # _, acc_dis, acc_cls = pt_learner.evaluate(model,
-  #                                       test_dataloader,
-  #                                       known_labels,
-  #                                       args)
-  # print('Dist: {}, Cls: {}'.format(acc_dis, acc_cls))
+  print('Test with last model')
+  _, acc_dis, acc_cls = pt_learner.evaluate(model,
+                                        test_dataloader,
+                                        known_labels,
+                                        args)
+  print('Dist: {}, Cls: {}'.format(acc_dis, acc_cls))
 
 
-  # print('Test with best model')
-  # try: model.load(args.best_model_path)
-  # except FileNotFoundError: pass
-  # else: print("Load model from {}".format(args.best_model_path))
-  # _, acc_dis, acc_cls = pt_learner.evaluate(model,
-  #                                       test_dataloader,
-  #                                       known_labels,
-  #                                       args)
-  # print('Dist: {}, Cls: {}'.format(acc_dis, acc_cls))
+  print('Test with best model')
+  try: model.load(args.best_model_path)
+  except FileNotFoundError: pass
+  else: print("Load model from {}".format(args.best_model_path))
+  _, acc_dis, acc_cls = pt_learner.evaluate(model,
+                                        test_dataloader,
+                                        known_labels,
+                                        args)
+  print('Dist: {}, Cls: {}'.format(acc_dis, acc_cls))
 
 
 
