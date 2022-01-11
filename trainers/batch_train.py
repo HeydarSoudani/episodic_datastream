2d1ff8e93f15c3a079c0ebd86212fb9dbbd5973d
Heydar Soudani <heydar.soudani@gmail.com>
2022-01-11 12:35:08 +0330

    sgd

--- trainers/batch_train.py
+++ trainers/batch_train.py
@@ -34,15 +34,15 @@ def train(model,
 
 
   criterion = torch.nn.CrossEntropyLoss()
-  # optim = SGD(model.parameters(), lr=args.lr)
+  optim = SGD(model.parameters(), lr=args.lr)
   # optim = SGD(model.parameters(),
   #             lr=args.lr,
   #             momentum=args.momentum,
   #             weight_decay=args.wd,  #l2 reg
   #             nesterov=True)
-  optim = Adam(model.parameters(),
-                lr=args.lr,
-                weight_decay=args.wd)
+  # optim = Adam(model.parameters(),
+  #               lr=args.lr,
+  #               weight_decay=args.wd)
 
   scheduler = StepLR(
     optim,
