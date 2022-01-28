d98453e33d60a1ac373e28011dd648e524e0f627
Heydar Soudani <heydar.soudani@gmail.com>
2022-01-28 12:20:59 +0330

    new stream builder

--- dataset_preparation/stream_builder.py
+++ dataset_preparation/stream_builder.py
@@ -84,13 +84,8 @@ if __name__ == '__main__':
   train_data = []
   test_data_seen = []
   for seen_class_item in seen_class:
-    
     train_idx = np.random.choice(class_data[seen_class_item].shape[0], args.spc, replace=False)
-    print(train_idx)
     seen_data = class_data[seen_class_item][train_idx]
-    # seen_data = np.array([class_data[seen_class_item][i] for i in train_idx])
-    print(seen_data.shape)
-
     class_data[seen_class_item] = np.delete(class_data[seen_class_item], train_idx, axis=0)
 
     # np.random.shuffle(seen_data)
@@ -99,17 +94,11 @@ if __name__ == '__main__':
     # train_part = seen_data[:last_idx]
     # test_part = seen_data[last_idx:]
 
-    # train_data_class = np.concatenate((train_part, np.full((last_idx , 1), seen_class_item)), axis=1)
-    # train_data.extend(train_data_class)
+    train_data_class = np.concatenate((seen_data, np.full((args.spc , 1), seen_class_item)), axis=1)
+    train_data.extend(train_data_class)
 
     # test_data_class = np.concatenate((test_part, np.full((test_data_length , 1), seen_class_item)), axis=1)
     # test_data_seen.extend(test_data_class)
-
-  for label, data in class_data.items():
-    print('Label: {} -> {}'.format(label, data.shape)) 
-
-  time.sleep(5)
-
 
   train_data = np.array(train_data) #(6000, 785)
   np.random.shuffle(train_data)
@@ -129,9 +118,9 @@ if __name__ == '__main__':
   chunk_size = 1000
   n_chunk = int(n_data / chunk_size)
   n_chunk_stream = n_chunk - 6
-
+  print(n_chunk_stream)
   add_new_class_points = np.random.choice(n_chunk_stream-20, len(unseen_class), replace=False)
-  print(novel_class_adding)
+  print(add_new_class_points)
   for i_chunk in range(n_chunk_stream):
 
     # add novel class to test data pool
