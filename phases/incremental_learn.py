fdc655dc94f96b50ed8adcf3d644cb01bf7a6825
Heydar Soudani <heydar.soudani@gmail.com>
2022-02-03 19:22:17 +0330

    forgetting added

--- phases/incremental_learn.py
+++ phases/incremental_learn.py
@@ -158,13 +158,16 @@ def increm_learn(model,
 
   print(temp)
 
-  forgetting_cls = torch.tensor([torch.mean(temp[:, i+1:])  for i in range(args.n_tasks-1)])
+  forgetting_cls = torch.tensor([torch.mean(temp[i+1:, i])  for i in range(args.n_tasks-1)])
 
   print(forgetting_cls)
 
   mean_forgetting_cls = torch.mean(forgetting_cls)
   std_forgetting_cls = torch.std(forgetting_cls)
   print('cls forgetting: {:.4f} ± {:.4f}'.format(mean_forgetting_cls, std_forgetting_cls))
+
+
+
 
   acc_dist_best = torch.max(all_tasks_acc_dist, 0).values
   temp = acc_dist_best - all_tasks_acc_dist
