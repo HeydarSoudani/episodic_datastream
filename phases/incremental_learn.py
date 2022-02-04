f04cfdcf6ddd170c8989165b2879004134c449d7
Heydar Soudani <heydar.soudani@gmail.com>
2022-02-04 11:27:08 +0330

    offline test added

--- phases/incremental_learn.py
+++ phases/incremental_learn.py
@@ -151,20 +151,20 @@ def increm_learn(model,
   all_tasks_acc_cls = torch.stack(all_tasks_acc_cls)
   all_tasks_acc_dist = torch.stack(all_tasks_acc_dist)
 
-  acc_cls_best = torch.max(all_tasks_acc_cls, 0).values
-  temp = acc_cls_best - all_tasks_acc_cls
-  forgetting_cls = torch.tensor([torch.mean(temp[i+1:, i]) for i in range(args.n_tasks-1)])
-
-  mean_forgetting_cls = torch.mean(forgetting_cls)
-  std_forgetting_cls = torch.std(forgetting_cls)
-  print('cls forgetting: {:.4f} ± {:.4f}'.format(mean_forgetting_cls, std_forgetting_cls))
-
   acc_dist_best = torch.max(all_tasks_acc_dist, 0).values
   temp = acc_dist_best - all_tasks_acc_dist
   forgetting_dist = torch.tensor([torch.mean(temp[i+1:, i]) for i in range(args.n_tasks-1)])
   mean_forgetting_dist = torch.mean(forgetting_dist)
   std_forgetting_dist = torch.std(forgetting_dist)
   print('dist forgetting: {:.4f} ± {:.4f}'.format(mean_forgetting_dist, std_forgetting_dist))
+
+  acc_cls_best = torch.max(all_tasks_acc_cls, 0).values
+  temp = acc_cls_best - all_tasks_acc_cls
+  forgetting_cls = torch.tensor([torch.mean(temp[i+1:, i]) for i in range(args.n_tasks-1)])
+  mean_forgetting_cls = torch.mean(forgetting_cls)
+  std_forgetting_cls = torch.std(forgetting_cls)
+  print('cls forgetting: {:.4f} ± {:.4f}'.format(mean_forgetting_cls, std_forgetting_cls))
+
 
 
 # def batch_increm_learn(model,
