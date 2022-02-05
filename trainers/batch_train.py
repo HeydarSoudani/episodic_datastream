b688e01b4fad8840a297a425e557108cca4fe2ef
Heydar Soudani <heydar.soudani@gmail.com>
2022-02-05 19:34:24 +0330

    add pretrain to c100

--- trainers/batch_train.py
+++ trainers/batch_train.py
@@ -36,11 +36,10 @@ def train(model,
   known_labels = val_dataset.label_set
 
   # == ====================================
-  # optim = Adam(model.parameters(), lr=args.lr)
-  optim = SGD(model.parameters(),
-              lr=args.lr,
-              momentum=args.momentum)
-  
+  optim = Adam(model.parameters(), lr=args.lr)
+  # optim = SGD(model.parameters(),
+  #             lr=args.lr,
+  #             momentum=args.momentum)
   scheduler = StepLR(optim, step_size=args.step_size, gamma=args.gamma)
 
   # == Learn model =========================
