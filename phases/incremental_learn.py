04c106318934340200d32b41e536238c618852d9
Heydar Soudani <heydar.soudani@gmail.com>
2022-03-05 11:20:53 +0330

    new acc testing added

--- phases/incremental_learn.py
+++ phases/incremental_learn.py
@@ -10,13 +10,53 @@ from datasets.dataset import SimpleDataset
 from utils.preparation import transforms_preparation
 
 
+def increm_test(model, current_task, args):
+  
+  tasks_acc_dist = [0.0 for _ in range(args.n_tasks)]
+  tasks_acc_cls = [0.0 for _ in range(args.n_tasks)]
+  
+  for prev_task in range(current_task+1):
+    test_data = pd.read_csv(
+                os.path.join(args.split_test_path, "task_{}.csv".format(prev_task)),
+                sep=',', header=None).values
+
+    if args.use_transform:
+      _, test_transform = transforms_preparation()
+      test_dataset = SimpleDataset(test_data, args, transforms=test_transform)
+    else:
+      test_dataset = SimpleDataset(test_data, args)
+    test_dataloader = DataLoader(dataset=test_dataset,
+                                  batch_size=args.batch_size,
+                                  shuffle=False)
+
+    # known_labels = test_dataset.label_set
+    # print('Test on: {}'.format(known_labels))
+    if args.dataset == 'cifar100':
+      known_labels = set(range((task+1)*5))
+    else:
+      known_labels = set(range((task+1)*2))
+    # print('Known_labels for task {} is: {}'.format(task, known_labels))
+    
+    _, acc_dis, acc_cls = learner.evaluate(model,
+                                          test_dataloader,
+                                          known_labels,
+                                          args)
+                 
+    tasks_acc_dist[prev_task] = acc_dis
+    tasks_acc_cls[prev_task] = acc_cls
+  
+  return task_acc_dist, task_acc_cls
+
+
 def increm_learn(model,
                   learner,
                   memory,
                   args, device):
 
-  all_tasks_acc_dist = []
-  all_tasks_acc_cls = []
+  # all_tasks_acc_dist = []
+  # all_tasks_acc_cls = []
+  all_dist_acc = {'task_{}'.format(i): [] for i in range(args.n_tasks)}
+  all_cls_acc = {'task_{}'.format(i): [] for i in range(args.n_tasks)}
   
   for task in range(args.n_tasks):  
     ### === Task data loading =====================
@@ -25,9 +65,9 @@ def increm_learn(model,
                   sep=',', header=None).values 
     print('task_data: {}'.format(task_data.shape))
 
-    ### === Split setting =========================
     if task != 0:
-
+      
+      ### === Split setting ===============
       if args.dataset == 'cifar100':
         if task == 5: args.ways = 20
       else:
@@ -112,65 +152,75 @@ def increm_learn(model,
     print('=== Testing ... ===')
     if args.which_model == 'best':
       model.load(args.best_model_path)
-    
-    tasks_acc_dist = [0.0 for _ in range(args.n_tasks)]
-    tasks_acc_cls = [0.0 for _ in range(args.n_tasks)]
 
-    for prev_task in range(task+1):
-      test_data = pd.read_csv(
-                  os.path.join(args.split_test_path, "task_{}.csv".format(prev_task)),
-                  sep=',', header=None).values
-
-      if args.use_transform:
-        _, test_transform = transforms_preparation()
-        test_dataset = SimpleDataset(test_data, args, transforms=test_transform)
-      else:
-        test_dataset = SimpleDataset(test_data, args)
-      test_dataloader = DataLoader(dataset=test_dataset,
-                                   batch_size=args.batch_size,
-                                   shuffle=False)
-
-      # known_labels = test_dataset.label_set
-      # print('Test on: {}'.format(known_labels))
-      if args.dataset == 'cifar100':
-        known_labels = set(range((task+1)*5))
-      else:
-        known_labels = set(range((task+1)*2))
-      # print('Known_labels for task {} is: {}'.format(task, known_labels))
-      
-      _, acc_dis, acc_cls = learner.evaluate(model,
-                                            test_dataloader,
-                                            known_labels,
-                                            args)
-      tasks_acc_dist[prev_task] = acc_dis
-      tasks_acc_cls[prev_task] = acc_cls
-    
-    all_tasks_acc_dist.append(torch.tensor(tasks_acc_dist))
-    all_tasks_acc_cls.append(torch.tensor(tasks_acc_cls))
+    tasks_acc_dist, tasks_acc_cls = increm_test(model, task, args)
+    for i in range(task):
+      all_dist_acc['task_{}'.format(i)].append(tasks_acc_dist[i])
+      all_cls_acc['task_{}'.format(i)].append(tasks_acc_cls[i])
 
     mean_acc_dist = np.mean(tasks_acc_dist[:task+1])
     mean_acc_cls = np.mean(tasks_acc_cls[:task+1])
-    # print("Dist acc.: %7.4f, %7.4f, %7.4f, %7.4f, %7.4f \n"% tuple(tasks_acc_dist))
-    # print("Cls  acc.: %7.4f, %7.4f, %7.4f, %7.4f, %7.4f \n"% tuple(tasks_acc_cls))
-    print("Dist acc.: %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f \n"% tuple(tasks_acc_dist))
-    print("Cls  acc.: %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f \n"% tuple(tasks_acc_cls))
+    
+    if args.dataset == 'cifar100': 
+      print("Dist acc.: %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f \n"% tuple(tasks_acc_dist))
+      print("Cls  acc.: %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f \n"% tuple(tasks_acc_cls))
+    else:
+      print("Dist acc.: %7.4f, %7.4f, %7.4f, %7.4f, %7.4f \n"% tuple(tasks_acc_dist))
+      print("Cls  acc.: %7.4f, %7.4f, %7.4f, %7.4f, %7.4f \n"% tuple(tasks_acc_cls))
     print('Mean -> Dist: {}, Cls: {}'.format(round(mean_acc_dist, 3), round(mean_acc_cls, 3)))
 
   ### == Claculate forgetting =================
-  all_tasks_acc_cls = torch.stack(all_tasks_acc_cls)
-  all_tasks_acc_dist = torch.stack(all_tasks_acc_dist)
+  # all_tasks_acc_cls = torch.stack(all_tasks_acc_cls)
+  # all_tasks_acc_dist = torch.stack(all_tasks_acc_dist)
 
-  acc_dist_best = torch.max(all_tasks_acc_dist, 0).values
-  temp = acc_dist_best - all_tasks_acc_dist
-  forgetting_dist = torch.tensor([torch.mean(temp[i+1:, i]) for i in range(args.n_tasks-1)])
-  mean_forgetting_dist = torch.mean(forgetting_dist)
-  std_forgetting_dist = torch.std(forgetting_dist)
-  print('dist forgetting: {:.4f} ± {:.4f}'.format(mean_forgetting_dist, std_forgetting_dist))
+  # acc_dist_best = torch.max(all_tasks_acc_dist, 0).values
+  # temp = acc_dist_best - all_tasks_acc_dist
+  # forgetting_dist = torch.tensor([torch.mean(temp[i+1:, i]) for i in range(args.n_tasks-1)])
+  # mean_forgetting_dist = torch.mean(forgetting_dist)
+  # std_forgetting_dist = torch.std(forgetting_dist)
+  # print('dist forgetting: {:.4f} ± {:.4f}'.format(mean_forgetting_dist, std_forgetting_dist))
 
-  acc_cls_best = torch.max(all_tasks_acc_cls, 0).values
-  temp = acc_cls_best - all_tasks_acc_cls
-  forgetting_cls = torch.tensor([torch.mean(temp[i+1:, i]) for i in range(args.n_tasks-1)])
-  mean_forgetting_cls = torch.mean(forgetting_cls)
-  std_forgetting_cls = torch.std(forgetting_cls)
-  print('cls forgetting: {:.4f} ± {:.4f}'.format(mean_forgetting_cls, std_forgetting_cls))
+  # acc_cls_best = torch.max(all_tasks_acc_cls, 0).values
+  # temp = acc_cls_best - all_tasks_acc_cls
+  # forgetting_cls = torch.tensor([torch.mean(temp[i+1:, i]) for i in range(args.n_tasks-1)])
+  # mean_forgetting_cls = torch.mean(forgetting_cls)
+  # std_forgetting_cls = torch.std(forgetting_cls)
+  # print('cls forgetting: {:.4f} ± {:.4f}'.format(mean_forgetting_cls, std_forgetting_cls))
 
+
+
+
+    # tasks_acc_dist = [0.0 for _ in range(args.n_tasks)]
+    # tasks_acc_cls = [0.0 for _ in range(args.n_tasks)]
+
+    # for prev_task in range(task+1):
+    #   test_data = pd.read_csv(
+    #               os.path.join(args.split_test_path, "task_{}.csv".format(prev_task)),
+    #               sep=',', header=None).values
+
+    #   if args.use_transform:
+    #     _, test_transform = transforms_preparation()
+    #     test_dataset = SimpleDataset(test_data, args, transforms=test_transform)
+    #   else:
+    #     test_dataset = SimpleDataset(test_data, args)
+    #   test_dataloader = DataLoader(dataset=test_dataset,
+    #                                batch_size=args.batch_size,
+    #                                shuffle=False)
+
+    #   # known_labels = test_dataset.label_set
+    #   # print('Test on: {}'.format(known_labels))
+    #   if args.dataset == 'cifar100':
+    #     known_labels = set(range((task+1)*5))
+    #   else:
+    #     known_labels = set(range((task+1)*2))
+    #   # print('Known_labels for task {} is: {}'.format(task, known_labels))
+      
+    #   _, acc_dis, acc_cls = learner.evaluate(model,
+    #                                         test_dataloader,
+    #                                         known_labels,
+    #                                         args)
+    #   tasks_acc_dist[prev_task] = acc_dis
+    #   tasks_acc_cls[prev_task] = acc_cls
+    
+    # all_tasks_acc_dist.append(torch.tensor(tasks_acc_dist))
+    # all_tasks_acc_cls.append(torch.tensor(tasks_acc_cls))
