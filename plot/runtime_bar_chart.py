a41ab59b91c5af73e40c927f9de2e9d2587e2c25
Heydar Soudani <heydar.soudani@gmail.com>
2022-03-09 21:46:58 +0330

    change metric loss to cont

--- plot/runtime_bar_chart.py
+++ plot/runtime_bar_chart.py
@@ -43,31 +43,34 @@ def main():
   width = 0.1
   
   fig, axs = plt.subplots(nrows=1, ncols=len(data), constrained_layout=True, figsize=(8,8))
-  for idx, item in enumerate(data):
-    dataset = item['dataset']
-    mean = item['mean']
-    std = item['std']
-    for i in range(n_methods):
-      index = ind + (i-2.5)*width 
-      axs[idx].bar(
-        index,
-        mean[i],
-        yerr=std[i],
-        color=colors[i],
-        width=width,
-        align='center',
-        label='{}'.format(methods[i])
+  
+  # for idx, item in enumerate(data):
+  #   dataset = item['dataset']
+  #   mean = item['mean']
+  #   std = item['std']
+  #   for i in range(n_methods):
+  #     index = ind + (i-2.5)*width 
+  #     axs[idx].bar(
+  #       index,
+  #       mean[i],
+  #       yerr=std[i],
+  #       color=colors[i],
+  #       width=width,
+  #       align='center',
+  #       label='{}'.format(methods[i])
     
-    axs[idx].set_xticks(ind)
-    axs[idx].set_xticklabels(['CPE', 'Metric', 'MetaLearning'], fontsize=8)
-    axs[idx].set_xlabel('Methods', fontsize=12)
-    axs[idx].set_ylabel('Run time (s)', fontsize=12)
-    axs[idx].set_title('{}'.format(dataset), fontsize=14, pad=50.0)
-    axs[idx].legend(loc='center', bbox_to_anchor=(0.5, 1.12),
-      fancybox=True, shadow=False, ncol=3, fontsize=11.5)
+  #   axs[idx].set_xticks(ind)
+  #   axs[idx].set_xticklabels(['CPE', 'Metric', 'MetaLearning'], fontsize=8)
+  #   axs[idx].set_xlabel('Methods', fontsize=12)
+  #   axs[idx].set_ylabel('Run time (s)', fontsize=12)
+  #   axs[idx].set_title('{}'.format(dataset), fontsize=14, pad=50.0)
+  #   axs[idx].legend(loc='center', bbox_to_anchor=(0.5, 1.12),
+  #     fancybox=True, shadow=False, ncol=3, fontsize=11.5)
 
-  # fig.savefig('bars.png', format='png', dpi=1400)
-  plt.show()
+  # # fig.savefig('bars.png', format='png', dpi=1400)
+  # plt.show()
 
 
 
+if __name__ == '__main__':
+  main()
