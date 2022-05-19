4deb8cccf9c6e0486006ff83ce31f5db29e93aea
HeydarS <heydar.soudani@gmail.com>
2022-05-19 18:07:46 +0430

    plot added

--- plot/feature_space_visualization.py
+++ plot/feature_space_visualization.py
@@ -22,9 +22,9 @@ def set_novel_label(known_labels, args, data=[]):
 
   for idx, item in enumerate(data):
     label = item[-1]
+    print(known_labels)
+    print(label)
     if label not in known_labels:
-      # print(known_labels)
-      # print(label)
       data[idx, -1] = 100
 
   return data
