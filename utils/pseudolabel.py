c528bd8a912867decb1827c5c65961823311848f
HeydarS <heydar.soudani@gmail.com>
2022-06-16 17:44:55 +0430

    plabel added

--- utils/pseudolabel.py
+++ utils/pseudolabel.py
@@ -14,11 +14,10 @@ def pseudo_labeler(data, n_component=2, ratio=1.0):
 	samples = torch.stack([item[0] for item in data])
 	labels = torch.tensor([item[1] for item in data])
 	features = torch.stack([item[2] for item in data])
-	seen_labels = torch.unique(labels)
 
 	gmm = GaussianMixture(n_components=n_component,  random_state=0)
-	gmm.fit(features)
-	gmm_predict = gmm.predict(features)
+	gmm.fit(features.detach().cpu().numpy())
+	gmm_predict = gmm.predict(features.detach().cpu().numpy())
 
 	for i in range(n_component):
 		component_idx = np.where(gmm_predict == i)[0].astype(int)
